c3f53827c58d1262e5aff037e550a3fa3dde9db7
AndreaErario <andreaerario04@gmail.com>
2021-07-01 10:32:03 +0200

    Adds Node class

---
+++ node.py
@@ -0,0 +1,13 @@
+import pygame
+
+class Node(object):
+    def __init__(self, win, x, y, size):
+        self.win = win
+        self.x = x
+        self.y = y
+        self.size = size
+        self.color = (255, 255, 255)
+
+    def draw(self):
+        pygame.draw.rect(self.win, self.color, (self.x, self.y, self.size, self.size))
+        pygame.draw.rect(self.win, (96, 96, 96), (self.x, self.y, self.size, self.size), 1)
