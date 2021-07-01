1718f1a34d1a60e7b28bcc098597cb8a240c4955
AndreaErario <andreaerario04@gmail.com>
2021-07-01 11:26:09 +0200

    Makes nodes obstacles on button press

--- main.py
+++ main.py
@@ -9,14 +9,29 @@ pygame.display.set_caption("A* algorithm visualization")
 
 run = True
 
+grid = []
+
+def draw():
+    win.fill((0, 0, 0))
+    for column in grid:
+        for node in column:
+            node.draw()
+    pygame.display.update()
+
+for x in range(win_size//node_size):
+        grid.append([])
+        for y in range(win_size//node_size):
+            grid[x].append(Node(win, x * node_size, y * node_size, node_size))
+
 while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
     
-    for x in range(win_size//node_size):
-        for y in range(win_size//node_size):
-            Node(win, x * node_size, y * node_size, node_size).draw()
-    pygame.display.update()
+    if pygame.mouse.get_pressed()[0]:
+        pos = pygame.mouse.get_pos()
+        grid[pos[0] // node_size][pos[1] // node_size].make_obstacle()
+    
+    draw()
 
 pygame.quit()
--- node.py
+++ node.py
@@ -8,6 +8,9 @@ class Node(object):
         self.size = size
         self.color = (255, 255, 255)
 
+    def make_obstacle(self):
+        self.color = (0, 0, 0)
+
     def draw(self):
         pygame.draw.rect(self.win, self.color, (self.x, self.y, self.size, self.size))
         pygame.draw.rect(self.win, (96, 96, 96), (self.x, self.y, self.size, self.size), 1)
