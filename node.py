61dfce6a3e970294cdf12da20a1f9bb8ee02e0d5
AndreaErario <andreaerario04@gmail.com>
2021-07-01 11:30:14 +0200

    Creates color variables

--- node.py
+++ node.py
@@ -1,4 +1,5 @@
 import pygame
+from assets.colors import *
 
 class Node(object):
     def __init__(self, win, x, y, size):
@@ -6,11 +7,11 @@ class Node(object):
         self.x = x
         self.y = y
         self.size = size
-        self.color = (255, 255, 255)
+        self.color = WHITE
 
     def make_obstacle(self):
-        self.color = (0, 0, 0)
+        self.color = BLACK
 
     def draw(self):
         pygame.draw.rect(self.win, self.color, (self.x, self.y, self.size, self.size))
-        pygame.draw.rect(self.win, (96, 96, 96), (self.x, self.y, self.size, self.size), 1)
+        pygame.draw.rect(self.win, GREY, (self.x, self.y, self.size, self.size), 1)
